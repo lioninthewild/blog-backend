5b53c655d6998083c23b084f60efc79a4b0a4b08
lioninthewild <dahal7prashish@gmail.com>
2026-04-05 21:48:49 +0545

    deleted app.use: no need of CORs

--- src/server.js
+++ src/server.js
@@ -5,9 +5,9 @@ app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  }),
-);
+// app.use(
+//   cors({
+//     origin: process.env.FRONTEND_URL,
+//     credentials: true,
+//   }),
+// );
