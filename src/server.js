3c359e66b49eec2089f8051e4e2e8c681eef8b6b
lioninthewild <dahal7prashish@gmail.com>
2026-04-05 21:34:05 +0545

    Add frontend URL to CORS config

--- src/server.js
+++ src/server.js
@@ -4,3 +4,10 @@ const { port } = require("./config/config");
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
